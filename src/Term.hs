1409c782ef0a40712410c05e3d419905c84f5067
Fangyi Zhou <fangyi.zhou15@imperial.ac.uk>
2017-05-06 18:03:04 +0100

    Pretty printing for terms

--- src/Term.hs
+++ src/Term.hs
@@ -4,4 +4,11 @@ data Term
   = Var String
   | Abs String Term
   | App Term Term
-  deriving Show
+
+instance Show Term where
+  show (Var v)
+    = v
+  show (Abs v t)
+    = "\\" ++ v ++ ". (" ++ show t ++ ")"
+  show (App t1 t2)
+    = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
