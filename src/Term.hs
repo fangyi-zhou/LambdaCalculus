6592000801d3109d84d951ac852055a0c011cc89
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-05-08 14:02:02 +0100

    Change printing scheme

--- src/Term.hs
+++ src/Term.hs
@@ -9,6 +9,6 @@ instance Show Term where
   show (Var v)
     = v
   show (Abs v t)
-    = "\\" ++ v ++ ". (" ++ show t ++ ")"
+    = "\\" ++ v ++ ". " ++ show t
   show (App t1 t2)
-    = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
+    = "(" ++ show t1 ++ ") (" ++ show t2 ++ ")"
