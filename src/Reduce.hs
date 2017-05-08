d4a2c3b585f3ba1e235fef5009e2150ac737614e
Fangyi Zhou <thomas.zhoufangyi@icloud.com>
2017-05-08 13:45:18 +0100

    Fix substitution capture

--- src/Reduce.hs
+++ src/Reduce.hs
@@ -1,5 +1,6 @@
 module Reduce where
 import Term
+import Data.List ((\\), union)
 
 reduce :: Term -> Term
 reduce (Abs var term)
@@ -12,9 +13,14 @@ reduce t
 replace :: Term -> String -> Term -> Term
 replace (Var v1) v2 t
   | v1 == v2  = t
-  | otherwise = (Var v1)
+  | otherwise = Var v1
 replace (Abs v1 term) v2 t
-  | v1 == v2  = (Abs v1 term)
-  | otherwise = Abs v1 (replace term v2 t)
+  | (v1 /= v2) && notElem v2 (freeVar t) = Abs v1 (replace term v2 t)
+  | otherwise = Abs v1 term
 replace (App t1 t2) v2 t
   = App (replace t1 v2 t) (replace t2 v2 t)
+
+freeVar :: Term -> [String]
+freeVar (Var v) = [v]
+freeVar (Abs var term) = freeVar term \\ [var]
+freeVar (App t1 t2) = freeVar t1 `union` freeVar t2
